0cf6e09b2282927ba43b349b057a378ad6e61fb6
gao-jinlong <2675130594@qq.com>
2023-12-08 00:09:25 +0800

    feat(scanner)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Java!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你的名字：");
+        String name = scanner.next();
+        System.out.println("你好，" + name + "!");
     }
 }
